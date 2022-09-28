773ff3a80db8a53c483e0334ba6e955a7744f10e
liuruinian <1394329227@qq.com>
2022-09-28 16:06:07 +0800

    新建模块：surpower-xxl-job

---
+++ surpower-xxl-job/xxl-job-core/src/main/java/io/github/liuruinian/xxl/core/biz/AdminBiz.java
@@ -0,0 +1,47 @@
+package io.github.liuruinian.xxl.core.biz;
+
+import io.github.liuruinian.xxl.core.biz.model.HandleCallbackParam;
+import io.github.liuruinian.xxl.core.biz.model.RegistryParam;
+import io.github.liuruinian.xxl.core.biz.model.ReturnT;
+import java.util.List;
+
+/**
+ * @author xuxueli 2017-07-27 21:52:49
+ */
+public interface AdminBiz {
+
+
+    // ---------------------- callback ----------------------
+
+    /**
+     * callback
+     *
+     * @param callbackParamList
+     * @return
+     */
+    public ReturnT<String> callback(List<HandleCallbackParam> callbackParamList);
+
+
+    // ---------------------- registry ----------------------
+
+    /**
+     * registry
+     *
+     * @param registryParam
+     * @return
+     */
+    public ReturnT<String> registry(RegistryParam registryParam);
+
+    /**
+     * registry remove
+     *
+     * @param registryParam
+     * @return
+     */
+    public ReturnT<String> registryRemove(RegistryParam registryParam);
+
+
+    // ---------------------- biz (custome) ----------------------
+    // group、job ... manage
+
+}
