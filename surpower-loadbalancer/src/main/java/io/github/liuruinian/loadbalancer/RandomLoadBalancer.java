1220ae8d0404738055bca670d9e3f0c6b51d97bb
liuruinian <1394329227@qq.com>
2022-10-25 16:58:23 +0800

    surpower-loadbalancer: finish RandomLoadBalancer

--- surpower-loadbalancer/src/main/java/io/github/liuruinian/loadbalancer/RandomLoadBalancer.java
+++ surpower-loadbalancer/src/main/java/io/github/liuruinian/loadbalancer/RandomLoadBalancer.java
@@ -3,14 +3,33 @@ package io.github.liuruinian.loadbalancer;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * random load balancer
+ *
+ * @param <T>
+ *         target class
+ * @param <C>
+ *         choose reference object
+ *
+ * @author liuruinian
+ */
 public class RandomLoadBalancer<T, C> extends AbstractLoadBalancer<T, C> {
 
     // ~ Constructors ---------------------------------------------------------
 
+    /**
+     * instantiation Load Balancer with CopyOnWriteArrayList
+     */
     public RandomLoadBalancer() {
         super();
     }
 
+    /**
+     * instantiation Load Balancer with appoint list
+     *
+     * @param targetList
+     *         target object list
+     */
     public RandomLoadBalancer(List<TargetWrapper<T>> targetList) {
         super(targetList);
     }
---
+++ surpower-loadbalancer/src/test/java/io/github/liuruinian/loadbalancer/TestLoadBalancer.java
@@ -0,0 +1,36 @@
+package io.github.liuruinian.loadbalancer;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TestLoadBalancer {
+
+    /**
+     * test random load balancer
+     */
+    @Test
+    public void testRandomLoadBalancer() {
+        List<TargetWrapper<String>> wrapperList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            TargetWrapper<String> targetWrapper = TargetWrapper.of("TW" + i);
+            targetWrapper.setActive(true);
+            wrapperList.add(targetWrapper);
+        }
+
+        RandomLoadBalancer<String, Object> loadBalancer = new RandomLoadBalancer<>(wrapperList);
+
+        for (int i = 0; i < 20; i++) {
+            String choose = loadBalancer.choose();
+            System.out.println("random load balancer choose: " + choose);
+            try {
+                TimeUnit.SECONDS.sleep(3L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
